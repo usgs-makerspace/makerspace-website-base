0d13e3665d63310d99ef0c3005c36c3574bfaa3b
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-07-06 17:06:05 -0500

    added dev window size and in-progress statement

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+    <WindowSize v-if="checkTypeOfEnv === '-test build-'" />
     <HeaderUSWDSBanner />
     <HeaderUSGS />
+    <WorkInProgressWarning v-if="checkReleaseState !== 'released'" />
     <router-view
       v-if="checkIfUSGSHeaderIsRendered"
       :is-internet-explorer="isInternetExplorer"
@@ -11,14 +13,17 @@
 </template>
 
 <script>
+    import WindowSize from "./components/WindowSize";
     import HeaderUSWDSBanner from './components/HeaderUSWDSBanner'
     import HeaderUSGS from './components/HeaderUSGS'
 
     export default {
         name: 'App',
         components: {
+            WindowSize,
             HeaderUSWDSBanner,
             HeaderUSGS,
+            WorkInProgressWarning: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "work-in-progress-warning"*/ "./components/WorkInProgressWarning"),
             FooterUSGS: () => import( /* webpackPrefetch: true */ /*webpackChunkName: "usgs-footer"*/ "./components/FooterUSGS") // Have Webpack put the footer in a separate chunk so we can load it conditionally (with a v-if) if we desire
         },
         data() {
@@ -31,11 +36,29 @@
             // note - this is an example pattern that can be used to control when any component loads in relation to other components
             checkIfUSGSHeaderIsRendered() {
                 return this.$store.state.usgsHeaderRendered;
+            },
+            checkTypeOfEnv() {
+                return process.env.VUE_APP_TIER
+            },
+            checkReleaseState() {
+                return process.env.VUE_APP_RELEASE_STATE
             }
         },
         created() {
             // We are ending support for Internet Explorer, so let's test to see if the browser used is IE.
             this.$browserDetect.isIE ? this.isInternetExplorer = true : this.isInternetExplorer = false;
+            // Add window size tracking by adding a listener and a way to store the values in the Vuex state
+            window.addEventListener('resize', this.handleResize);
+            this.handleResize();
+        },
+        destroyed() {
+            window.removeEventListener('resize', this.handleResize);
+        },
+        methods: {
+            handleResize() {
+                this.$store.commit('recordWindowWidth', window.innerWidth);
+                this.$store.commit('recordWindowHeight', window.innerHeight);
+            }
         }
     }
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -5,11 +5,19 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        usgsHeaderRendered: false
+        usgsHeaderRendered: false,
+        windowWidth: 0,
+        windowHeight: 0,
     },
     mutations: {
         changeBooleanStateWhenUSGSHeaderRendered(state) {
             state.usgsHeaderRendered = true
+        },
+        recordWindowWidth (state, payload) {
+            state.windowWidth = payload
+        },
+        recordWindowHeight (state, payload) {
+            state.windowHeight = payload
         }
     }
 });
